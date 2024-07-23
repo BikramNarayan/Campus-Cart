f8b5b2dd97184adf4597e9e97b15781bb9330ab1
BikramNarayan <lcb2022029@iiitl.ac.in>
2024-07-23 20:02:28 +0530

    Minor changes

--- src/pages/Your_blogs.js
+++ src/pages/Your_blogs.js
@@ -9,7 +9,6 @@ import {
   doc,
 } from "firebase/firestore";
 import { firestore } from "../firebase";
-import { Link } from "react-router-dom";
 import amazon from "../assets/combo1.png";
 import flipkart from "../assets/combo2.jpeg";
 import blinkit from "../assets/combo3.jpeg";
@@ -132,165 +131,191 @@ const YourBlogs = () => {
     }
   };
 
+  if (isLoading) return null;
+
   return (
     <>
-      <div
-        style={{
-          background: "#DCF8AB",
-        }}
-      >
-        <h1
-          className="mt-2"
-          style={{
-            width: "fit-content",
-            margin: "auto",
-          }}
-        >
-          Your Blogs
-        </h1>
-      </div>
-      <div className="container ride">
-        {loadingProducts || loadingOrders || loadingRides ? (
+      {isAuthenticated ? (
+        <>
           <div
-            className="d-flex justify-content-center align-items-center"
-            style={{ height: "60vh" }}
+            style={{
+              background: "#DCF8AB",
+            }}
           >
-            <div className="spinner-border text-success" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </div>
+            <h1
+              className="mt-2"
+              style={{
+                width: "fit-content",
+                margin: "auto",
+              }}
+            >
+              Your Blogs
+            </h1>
           </div>
-        ) : (
-          <>
-            {products.length === 0 ? (
-              <h2 className="mt-2">You don't have any product blogs</h2>
+          <div className="container ride">
+            {loadingProducts || loadingOrders || loadingRides ? (
+              <div
+                className="d-flex justify-content-center align-items-center"
+                style={{ height: "60vh" }}
+              >
+                <div className="spinner-border text-success" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              </div>
             ) : (
               <>
-                <h1 className="mt-2">Your Products</h1>
-                <div className="row row-cols-2 row-cols-md-2 row-cols-lg-4 g-0">
-                  {products.map((product) => (
-                    <div key={product.id} className="col mb-4">
-                      <div className="card card1">
-                        <img
-                          src={product.imageUrl}
-                          className="card-img-top"
-                          alt={product.name}
-                        />
-                        <div className="card-body" style={{ paddingBottom: 0 }}>
-                          <h5 className="card-title">
-                            <strong>{product.name}</strong>
-                          </h5>
-                          <p className="card-text">
-                            <strong>{product.sellerNm}</strong>
-                          </p>
-                          <p className="card-text">
-                            <strong>Hostle:</strong> {product.location}
-                          </p>
-                          <h5 className="">Rs {product.price.toFixed(0)}</h5>
-                          <div className="col-lg-6">
+                {products.length === 0 ? (
+                  <h2 className="mt-2">You don't have any product blogs</h2>
+                ) : (
+                  <>
+                    <h1 className="mt-2">Your Products</h1>
+                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-0">
+                      {products.map((product) => (
+                        <div key={product.id} className="col mb-4">
+                          <div className="card card1">
+                            <img
+                              src={product.imageUrl}
+                              className="card-img-top"
+                              alt={product.name}
+                            />
+                            <div
+                              className="card-body"
+                              style={{ paddingBottom: 0 }}
+                            >
+                              <h5 className="card-title">
+                                <strong>{product.name}</strong>
+                              </h5>
+                              <p className="card-text">
+                                <strong>{product.sellerNm}</strong>
+                              </p>
+                              <p className="card-text">
+                                <strong>Hostel:</strong> {product.location}
+                              </p>
+                              <h5 className="">
+                                Rs {product.price.toFixed(0)}
+                              </h5>
+                              <div className="col-lg-6">
+                                <button
+                                  onClick={() =>
+                                    handleDelete("products", product.id)
+                                  }
+                                  className="btn border-danger border-2 btn-danger w-100"
+                                >
+                                  Delete
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                )}
+                {orders.length === 0 ? (
+                  <h1 className="mt-2">
+                    You don't have any Combine Orders Blog
+                  </h1>
+                ) : (
+                  <>
+                    <h1 className="mt-2">Your Orders</h1>
+                    <div className="row row-cols-1 row-cols-md-3 g-3">
+                      {orders.map((listing) => (
+                        <div key={listing.id} className="col mb-4">
+                          <div className="card card1" style={{ width: "98%" }}>
+                            <img
+                              src={getBrandBadge(listing.brandName)}
+                              className="card-img-top"
+                              alt={listing.brandName}
+                              style={{
+                                width: "150px",
+                                height: "50px",
+                                margin: "5px",
+                              }}
+                            />
+                            <div
+                              className="card-body"
+                              style={{ paddingBottom: 0 }}
+                            >
+                              <h5 className="card-title">
+                                <strong>{listing.brandName}</strong>
+                              </h5>
+                              <p className="card-text">
+                                Offer: {listing.offer}
+                              </p>
+                              <p className="card-text">
+                                Amount Required: ₹{listing.amountRequired}
+                              </p>
+                              <p className="card-text">
+                                Posted by: {listing.name}
+                              </p>
+                              <p className="card-text">
+                                Phone: {listing.phoneNumber}
+                              </p>
+                              <button
+                                onClick={() =>
+                                  handleDelete("comb_order", listing.id)
+                                }
+                                className="btn border-danger border-2 btn-danger w-100"
+                              >
+                                Delete
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                )}
+                {rideData.length === 0 ? (
+                  <h1 className="mt-2">You don't have any Combine Ride Blog</h1>
+                ) : (
+                  <>
+                    <h1 className="mt-2">Your Rides</h1>
+                    <div className="row row-cols-1 row-cols-md-3 g-3">
+                      {rideData.map((listing) => (
+                        <div key={listing.id} className="col mb-4">
+                          <div className="card card1">
+                            <div
+                              className="card-body"
+                              style={{ padding: "20px" }}
+                            >
+                              <h5 className="card-title">
+                                {listing.startingPoint} to {listing.endPoint}
+                              </h5>
+                              <p className="card-text block">
+                                Date: {listing.date}
+                              </p>
+                              <p className="card-text block">
+                                Passenger required : {listing.passengerCount}
+                              </p>
+                              <p className="card-text block">
+                                Via : {listing.type}
+                              </p>
+                            </div>
                             <button
                               onClick={() =>
-                                handleDelete("products", product.id)
+                                handleDelete("ride-sharing", listing.id)
                               }
-                              className="btn border-danger border-2 btn-danger w-100"
+                              style={{ width: "fit-content", margin: "auto" }}
+                              className="btn border-danger border-2 btn-danger w-50"
                             >
                               Delete
                             </button>
                           </div>
                         </div>
-                      </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
+                  </>
+                )}
+                <br />
+                <br />
               </>
             )}
-            {orders.length === 0 ? (
-              <h2>You don't have any Combine Orders Blog</h2>
-            ) : (
-              <>
-                <h2>Your Orders</h2>
-                <div className="row row-cols-2 row-cols-md-3 g-3">
-                  {orders.map((listing) => (
-                    <div key={listing.id} className="col mb-4">
-                      <div className="card card1" style={{ width: "98%" }}>
-                        <img
-                          src={getBrandBadge(listing.brandName)}
-                          className="card-img-top"
-                          alt={listing.brandName}
-                          style={{
-                            width: "150px",
-                            height: "50px",
-                            margin: "5px",
-                          }}
-                        />
-                        <div className="card-body" style={{ paddingBottom: 0 }}>
-                          <h5 className="card-title">
-                            <strong>{listing.brandName}</strong>
-                          </h5>
-                          <p className="card-text">Offer: {listing.offer}</p>
-                          <p className="card-text">
-                            Amount Required: ₹{listing.amountRequired}
-                          </p>
-                          <p className="card-text">Posted by: {listing.name}</p>
-                          <p className="card-text">
-                            Phone: {listing.phoneNumber}
-                          </p>
-                          <button
-                            onClick={() =>
-                              handleDelete("comb_order", listing.id)
-                            }
-                            className="btn border-danger border-2 btn-danger w-100"
-                          >
-                            Delete
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </>
-            )}
-            {rideData.length === 0 ? (
-              <h2>You don't have any Combine Ride Blog</h2>
-            ) : (
-              <>
-                <h2>Your Rides</h2>
-                <div className="row row-cols-1 row-cols-md-3 g-3">
-                  {rideData.map((listing) => (
-                    <div key={listing.id} className="col mb-4">
-                      <div className="card card1">
-                        <div className="card-body" style={{ padding: "20px" }}>
-                          <h5 className="card-title">
-                            {listing.startingPoint} to {listing.endPoint}
-                          </h5>
-                          <p className="card-text block">
-                            Date: {listing.date}
-                          </p>
-                          <p className="card-text block">
-                            Passenger required : {listing.passengerCount}
-                          </p>
-                          <p className="card-text block">
-                            Via : {listing.type}
-                          </p>
-                        </div>
-                        <button
-                          onClick={() =>
-                            handleDelete("ride-sharing", listing.id)
-                          }
-                          className="btn border-danger border-2 btn-danger w-50"
-                        >
-                          Delete
-                        </button>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </>
-            )}
-            <br />
-            <br />
-          </>
-        )}
-      </div>
+          </div>
+        </>
+      ) : (
+        <h1>Please Login to See Blogs</h1>
+      )}
     </>
   );
 };
